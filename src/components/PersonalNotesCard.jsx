9faba1b131d1bb9a4aebe9c150810604d729b793
kenaneroz <kenaneroz@stu.khas.edu.tr>
2026-07-07 15:37:10 +0300

    style: make minor UI change

--- src/components/PersonalNotesCard.jsx
+++ src/components/PersonalNotesCard.jsx
@@ -4,7 +4,7 @@ export default function PersonalNotesCard({ note, page, date }) {
 
     return (
         <div className="border-l-[3px] border-tan pl-3 py-1">
-            <p className="text-coffee text-body italic">"{note}"</p>
+            <p className="text-coffee text-body">"{note}"</p>
             <div className="flex gap-4 justify-end mt-2">
                 {page !== "" && <p className="text-taupe text-body-xs">p. {page}</p>}
                 <p className="text-taupe text-body-xs">{date_}</p>
