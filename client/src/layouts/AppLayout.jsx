6b363fad9697df64a6e9cca67468099a2a08f2fb
kenaneroz <kenaneroz@stu.khas.edu.tr>
2026-08-29 11:14:47 +0300

    style: UI changes

--- client/src/layouts/AppLayout.jsx
+++ client/src/layouts/AppLayout.jsx
@@ -1,9 +1,9 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom'
 
 export default function AppLayout() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-tan">
-      <div className="w-full md:w-110 h-dvh md:h-239 bg-cream flex flex-col overflow-hidden relative">
+    <div className="bg-tan">
+      <div className="w-full md:w-110 h-dvh md:max-h-239 bg-cream flex flex-col overflow-hidden relative">
         <Outlet />
       </div>  
     </div>
